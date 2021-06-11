300210fd22483f8aec80bf2f76b19823fd5a14bb
LeeJinWoo <dlwlsdn201@naver.com>
2021-06-11 21:10:16 +0900

    [feature] usePromise Custom hook 생성

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import NewsItem from './NewsItem';
 import styled from 'styled-components';
-
+import usePromise from '../lib/usePromise';
 
 const NewsListContainer = styled.div`
     display: flex;
@@ -10,9 +10,10 @@ const NewsListContainer = styled.div`
     justify-content: center;
     align-items: center;
     width: 100%;
+    height: 100%;
+    color: white;
     border: 1px solid blue;
     border-radius: 10px; 
-    height: 100%;
 `;
 
 const NewsListWrapper = styled.ul`
@@ -28,41 +29,58 @@ const NewsListWrapper = styled.ul`
 
 
 const NewsList = ({category}) => {
+
     const API_KEY = 'f72290af08794eafb2893b02b4f21a5e';
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
 
-    useEffect(()=> {
-        //async를 사용하는 함수를 따로 선언.
-
-        const LoadData = async () => {
-            setLoading(true);
-            try{
-                const query = category === 'all' ? '' : `&category=${category}`;
-                const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${API_KEY}`,);
-                setData(response.data.articles);
-                console.log(response.data.articles);
-            }catch(e){
-                console.log(`error:${e}`);
-            }
-            setLoading(false);
-        };
-        LoadData();
+    const [loading, response, error] = usePromise(()=>{
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${API_KEY}`,);
     }, [category]);
+
+    // const [data, setData] = useState(null);
+    // const [loading, setLoading] = useState(false);
+
+    // useEffect(()=> {
+    //     //async를 사용하는 함수를 따로 선언.
+
+    //     const LoadData = async () => {
+    //         setLoading(true);
+    //         try{
+    //             // const query = category === 'all' ? '' : `&category=${category}`;
+    //             // const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${API_KEY}`,);
+    //             setData(response.data.articles);
+    //             console.log(response.data.articles);
+    //         }catch(e){
+    //             console.log(`error:${e}`);
+    //         }
+    //         setLoading(false);
+    //     };
+    //     LoadData();
+    // }, [category]);
 
     // console.log(data);
     if(loading){
         return <NewsListContainer>데이터 로드 중..</NewsListContainer>
     };
 
-    if(!data) {
+    //아직 response가 정의되지 않았을 때
+    if(!response) {
         return null;
     }
 
+    if (error) {
+        return <NewsListContainer>에러 발생</NewsListContainer>
+    }
+
+    // if(!data) {
+    //     return null;
+    // }
+
+    const { articles } = response.data;
     return (
         <NewsListContainer>
             <NewsListWrapper>
-                {data.map(article=> (<NewsItem key={article.url} article={article}/>))}
+                {articles.map(article=> (<NewsItem key={article.url} article={article}/>))}
             </NewsListWrapper>
         </NewsListContainer>
     );
